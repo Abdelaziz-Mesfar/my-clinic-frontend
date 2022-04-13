6701cc18ac0acb689f2083cb3aa74b09a88e72a9
Abdelaziz Mesfar <abdelaziz.mesfar01@gmail.com>
2022-04-13 02:45:45 +0100

    fetch patients in the patients page

--- src/pages/Patients.js
+++ src/pages/Patients.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function Patients() {
+  const [patients, setPatient] = useState([])
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/patients`)
+      .then(res => {
+        setPatient(res.data)
+      })
+      .catch(err => {
+        alert(err.message)
+      })
+  }, [])
   return (
-    <div>Patients</div>
+    <>
+      <div>Patients</div>
+      {
+        patients.map(patient => (
+          <div className="card" key={patient._id}>
+            <h1> {patient.firstName} </h1>
+          </div>
+        ))
+      }
+    </>
   )
 }
 
