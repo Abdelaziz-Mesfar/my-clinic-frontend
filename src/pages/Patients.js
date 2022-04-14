e98090fd563b5595170277d110f09c0dc90b6ad5
Abdelaziz Mesfar <abdelaziz.mesfar01@gmail.com>
2022-04-14 01:45:25 +0100

    fetch patients via calling an async action creator

--- src/pages/Patients.js
+++ src/pages/Patients.js
@@ -1,21 +1,22 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { setAllPatients } from '../redux/actions/patientsActionCreator'
+import { fetchAllPatients, setAllPatients } from '../redux/actions/patientsActionCreator'
 
 function Patients() {
   // const [patients, setPatient] = useState([])
   const patients = useSelector(state => state.patients.all)
   const dispatch = useDispatch()
   useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}/patients`)
-      .then(res => {
-        // setPatient(res.data)
-        dispatch(setAllPatients(res.data))
-      })
-      .catch(err => {
-        alert(err.message)
-      })
+    // axios.get(`${process.env.REACT_APP_API_URL}/patients`)
+    //   .then(res => {
+    //     // setPatient(res.data)
+    //     dispatch(setAllPatients(res.data))
+    //   })
+    //   .catch(err => {
+    //     alert(err.message)
+    //   })
+    dispatch(fetchAllPatients())
   }, [])
   return (
     <>
--- src/redux/actions/patientsActionCreator.js
+++ src/redux/actions/patientsActionCreator.js
@@ -1,6 +1,19 @@
+import axios from "axios";
 import { SET_ALL_PATIENTS } from "../types/patientActionTypes";
 
 export const setAllPatients = (patientsArray) => ({
     type: SET_ALL_PATIENTS,
     payload: patientsArray
 })
+
+export const fetchAllPatients = () => {
+    return async (dispatch) => {
+        try {
+            const res = await axios.get(`${process.env.REACT_APP_API_URL}/patients`)
+            const patients = res.data
+            dispatch(setAllPatients(patients))
+        } catch (error) {
+            console.log({ error });
+        }
+    }
+}
