4e02b81be18eb142f725f99ea2f136ec1586bc43
Abdelaziz Mesfar <abdelaziz.mesfar01@gmail.com>
2022-04-17 02:48:18 +0100

    logout from the side bar

--- src/components/side bar/Sidebar.js
+++ src/components/side bar/Sidebar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import {
     CDBSidebar,
     CDBSidebarContent,
@@ -7,11 +9,12 @@ import {
     CDBSidebarMenuItem,
     CDBSidebarFooter,
 } from 'cdbreact';
-import { NavLink } from 'react-router-dom';
 
 import './sidebar.css'
+import { logout } from '../../redux/actions/userActionCreators';
 
 const Sidebar = () => {
+    const dispatch = useDispatch()
     return (
         <CDBSidebar style={{ height: "100vh", position: "sticky", top: 0 }} textColor="#333" backgroundColor="#f0f0f0">
             <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
@@ -32,7 +35,9 @@ const Sidebar = () => {
             </CDBSidebarContent>
 
             <CDBSidebarFooter style={{ textAlign: 'center' }}>
-                <CDBSidebarMenuItem icon="bi bi-door-open-fill" className="text-center" >Log Out</CDBSidebarMenuItem>
+                <CDBSidebarMenuItem icon="bi bi-door-open-fill" className="text-center" onClick={()=> dispatch(logout())} >
+                    Log Out
+                </CDBSidebarMenuItem>
             </CDBSidebarFooter>
         </CDBSidebar>
     );
