53f9a6d53de565a6499bfc843acf4b194eb5d4a2
Abdelaziz Mesfar <abdelaziz.mesfar01@gmail.com>
2022-07-11 15:59:08 +0100

    add navigation link to the landing page in the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import  Nav  from 'react-bootstrap/Nav'
-import  Container  from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import BNavbar from 'react-bootstrap/Navbar'
 import { NavLink } from 'react-router-dom'
 
+import './navbar.css'
+
 function Navbar() {
-  return (
-    <BNavbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    return (
+        <BNavbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <BNavbar.Brand >My Clinic</BNavbar.Brand>
+                <BNavbar.Brand >
+                    <NavLink to="/" className="navbar-app-name">
+                        MyClinic
+                    </NavLink>
+                </BNavbar.Brand>
                 <BNavbar.Toggle aria-controls="responsive-navbar-nav" />
                 <BNavbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">                        
+                    <Nav className="me-auto">
                     </Nav>
                     <Nav>
                         <NavLink className="nav-link" to="/login" >Login</NavLink>
@@ -20,7 +26,7 @@ function Navbar() {
                 </BNavbar.Collapse>
             </Container>
         </BNavbar>
-  )
+    )
 }
 
 export default Navbar
